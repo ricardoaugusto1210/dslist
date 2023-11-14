3840c632c7489bb39b7516152c0767199e87d972
ricardoaugusto1210 <ricardoaugusto.eb@gmail.com>
2023-11-13 23:14:05 -0300

    Project created

---
+++ src/test/java/com/ricardoaugusto/dslit/DslitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardoaugusto.dslit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
